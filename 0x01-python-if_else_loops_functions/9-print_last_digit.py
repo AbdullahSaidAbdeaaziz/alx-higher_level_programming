67df4c69e9a0304fc495baf71bea1f3a94b7544b
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-16 16:56:16 +0200

    fix format

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         number = -number
-        return -(number % 10)
+        print("{}".format(number % 10), end="")
+        return number % 10
+    print("{}".format(number % 10), end="")
     return number % 10
