0d791494ed43b13db4902b0c0e7e6ea79abff99c
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-16 16:44:44 +0200

    tsk9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def uppercase(c):
-    return 65 <= ord(c) <= 90
+def uppercase(str):
+    upper_str = ""
+    for c in str:
+        if not c.islower():
+            upper_str += c
+            continue
+        upper_char = ord(c) - 32
+        upper_str += chr(upper_char)
+    print(upper_str)
