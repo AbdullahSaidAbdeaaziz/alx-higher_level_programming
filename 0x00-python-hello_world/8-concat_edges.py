810aec43b5b8ff89a7427ece44826b255f7c8a75
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-16 13:18:52 +0200

    haaaaaaaaaaa

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object"):].split()[0], "with Python " +
-      str[str.find("object"):].split()[1])
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
