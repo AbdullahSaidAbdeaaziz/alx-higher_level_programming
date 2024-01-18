2e58ec718e67577d87db53ff32fe0e162b588494
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-18 15:13:00 +0200

    fix output fking "s"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     number_args = len(argv) - 1
     if not number_args:
         print("{} arguments.".format(number_args))
+    elif number_args == 1:
+        print("{} argument:".format(number_args))
     else:
         print("{} arguments:".format(number_args))
     for i, v in enumerate(argv[1:]):
