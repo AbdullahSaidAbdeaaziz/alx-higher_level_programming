0695a651fe08d57e48267d93d2bf18d19b86af66
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-21 22:51:13 +0200

    feat:adding 8 task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a ==():
+    if tuple_a == ():
         tuple_a = (0, 0)
-    if tuple_b==():
+    if tuple_b == ():
         tuple_b = (0, 0)
     if len(tuple_a) < 2:
         tuple_a = (tuple_a[0], 0)
