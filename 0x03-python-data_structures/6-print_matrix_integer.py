09527f4ca13ebd57410753e0d7b446c100bf5b60
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-21 22:36:32 +0200

    fix: adding missing :d

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         for i, col in enumerate(row):
-            print("{}".format(col), end='')
+            print("{:d}".format(col), end='')
             if i != len(row) - 1:
                 print(end=" ")
         print()
