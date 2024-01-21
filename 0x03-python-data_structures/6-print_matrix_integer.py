5884e4938d3be37b0529c31a66193ba1bf258ca2
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-21 22:33:01 +0200

    feat:Adding 7 task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for row in matrix:
+        for i, col in enumerate(row):
+            print(col, end='')
+            if i != len(row) - 1:
+                print(end=" ")
+        print()
