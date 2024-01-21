081aa9bb2c68f31e5095027ec7ae11a0d9268a9e
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-21 22:03:02 +0200

    try to except what corrent output

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
     reversed_list = my_list[::-1]
     for i in reversed_list:
         print(
