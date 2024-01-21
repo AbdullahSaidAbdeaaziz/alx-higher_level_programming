69497f6e562218b0fac4df54c3a18d695dc0af26
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2024-01-21 23:14:03 +0200

    add: 10 task

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    _max = -1
+    for i in my_list:
+        if i > _max:
+            _max = i
+    return _max
