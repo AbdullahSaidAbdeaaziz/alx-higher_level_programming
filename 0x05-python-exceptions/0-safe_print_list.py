9f38c0fd978a8889785dc61799b62b65361aea43
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-06 02:01:47 +0200

    try in another way

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,11 +10,10 @@ def _size(my_list):
 def safe_print_list(my_list=[], x=0):
     try:
         size = _size(my_list)
-        if x > size:
-            x = size
         for i in range(x):
             print("{}".format(my_list[i]), end="")
         print()
         return x
-    except Exception:
+    except IndexError:
+        print()
         return size
