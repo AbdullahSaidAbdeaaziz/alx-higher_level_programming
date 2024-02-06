25a96cd34fd53199e5271aa6345f224a628d6eb9
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-06 04:02:29 +0200

    solve magic function I love bytecode of python

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from dis import dis
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("too far")
+            result += a ** b / i
+        except Exception:
+            result = a + b
+            break
+        return result
+
+print(dis(magic_calculation))
