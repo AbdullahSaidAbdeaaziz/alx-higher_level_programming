074df64008dec3e7db589b4c1e025afc9240820d
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-06 02:12:55 +0200

    fix others data types

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 
 def safe_print_integer(value):
     try:
-        value = int(value)
+        if not isinstance(value, int):
+            raise ValueError
         print("{:d}".format(value))
         return True
     except ValueError:
