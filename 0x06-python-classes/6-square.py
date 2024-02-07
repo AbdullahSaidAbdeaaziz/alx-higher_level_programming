2bd46a4b2ce7f62b6febb677b87b4bef9852a1b0
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-07 22:36:41 +0200

    hope

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,7 +53,8 @@ class Square:
             print("")
             return
 
-        for i in self.__position[1]:
-            print("")
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            print(f"{' '*self.__position[0]}{'#'*self.__size}")
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
