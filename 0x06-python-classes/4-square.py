9f4fc7817dfe7fa58042bd0e3401eb7ebc2ede26
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-07 21:19:26 +0200

    add: tsk5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+'''More upgrade for class square'''
+
+
+class Square:
+    "Hi I'm upgrade square"
+    def __init__(self, size=0) -> None:
+        '''Initialize square with size also with default value.
+
+        Args:
+            size (int): size of square
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''get size of square'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''set value to size of square'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''Return area of square'''
+        return self.__size ** 2
