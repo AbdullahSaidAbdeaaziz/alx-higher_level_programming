c3b9b95c15e8fff9e7dca51e902ca18d7b289f63
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-07 22:07:19 +0200

    whats wrong

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,5 +52,5 @@ class Square:
             print("")
             return
         print(""*self.position[1])
-        for i in range(self.__size):
-            print(" "*self.__position[0], "#"*self.__size, sep="")
+        for _ in range(self.__size):
+            print(f'{" "*self.__position[0]}{"#"*self.__size}')
