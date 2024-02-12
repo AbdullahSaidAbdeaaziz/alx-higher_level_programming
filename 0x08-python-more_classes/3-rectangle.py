2ecbc922fcbfc51695ab2fd2a1df44266f97a33e
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-12 21:14:03 +0200

    fix pystyle format

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -69,6 +69,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ''
