6d1ed0e876e5a62c06c6d411a7fde369bf10d2d7
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-14 10:39:21 +0200

    adding doc

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+'''Define LockedClass'''
+
 
 class LockedClass:
+    '''Prevent last_name attribute from creating'''
+
     def __setattr__(self, __name, __value):
+        '''
+        check instance name of attribute that begin pass to class
+        Raises:
+        AttributeError: class has no attribute last_name
+        '''
+
         if __name == 'last_name':
             raise AttributeError("'LockedClass' has no attribute 'last_name'")
