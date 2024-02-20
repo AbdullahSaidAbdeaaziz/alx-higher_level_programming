f04da220dfac3706c159daac3a38cb87acfeb44b
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-20 23:01:51 +0200

    add 8 task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''adds all arguments to a Python list,
+and then save them to a file
+'''
+from sys import argv
+
+
+def main():
+    load_file = __import__('6-load_from_json_file').load_from_json_file
+    save_file = __import__('5-save_to_json_file').save_to_json_file
+    
+    try:
+        items = load_file('add_item.json')
+    except Exception:
+        items = []
+    finally:
+        items += argv[1:]
+    save_file(items, 'add_item.json')
+
+
+if __name__ == '__main__':
+    main()
