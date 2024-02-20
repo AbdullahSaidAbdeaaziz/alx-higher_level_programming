d70446c1fce1429e397434140fc1ecf0521a41e8
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-20 22:44:49 +0200

    fix function parameter

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,7 +3,7 @@
 from json import load
 
 
-def load_from_json_file(my_obj, filename):
+def load_from_json_file(filename):
     '''put json file in object to deal it later'''
     with open(filename) as json_file:
         return load(json_file)
