51c762f553f7d5fa0072243c7259f253448f2fed
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-20 22:38:39 +0200

    fix 7 in task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-'''Read specific file'''
+filename = "my_list.json"
+my_list = load_from_json_file(filename)
+print(my_list)
+print(type(my_list))
 
+filename = "my_dict.json"
+my_dict = load_from_json_file(filename)
+print(my_dict)
+print(type(my_dict))
 
-def read_file(filename=""):
-    '''read file if exist, otherwise raise error'''
-    if not filename:
-        raise FileNotFoundError("file doesn't exist")
-    with open(filename, 'r', encoding='utf-8') as file:
-        stdout = file.read()
-        print(stdout, end="")
+try:
+    filename = "my_set_doesnt_exist.json"
+    my_set = load_from_json_file(filename)
+    print(my_set)
+    print(type(my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    filename = "my_fake.json"
+    my_fake = load_from_json_file(filename)
+    print(my_fake)
+    print(type(my_fake))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,5 @@ from json import loads
 
 def load_from_json_file(my_obj, filename):
     '''put json file in object to deal it later'''
-    with open(filename, 'r') as json_file:
-        content = json_file.read()
-    return loads(content)
+    with open(filename) as json_file:
+        return loads(json_file)
