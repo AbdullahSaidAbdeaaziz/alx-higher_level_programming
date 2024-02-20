89543ee202b11b8532a8d49476234de8ec3213f9
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-20 21:26:40 +0200

    add first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Read specific file'''
+
+def read_file(filename=""):
+    '''read file if exist, otherwise raise error'''
+    if not filename:
+        raise FileNotFoundError("file doesn't exist")
+    with open(filename, 'r', encoding='utf-8') as file:
+        stdout = file.read()
+        print(stdout, end="")
