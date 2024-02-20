984793928dff3639ced03dfbbb6467962d1234d8
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-20 22:41:02 +0200

    fix 7 in task

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''load object contains json from file'''
-from json import loads
+from json import load
 
 
 def load_from_json_file(my_obj, filename):
     '''put json file in object to deal it later'''
     with open(filename) as json_file:
-        return loads(json_file)
+        return load(json_file)
