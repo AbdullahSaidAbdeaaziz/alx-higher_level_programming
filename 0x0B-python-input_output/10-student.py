d36535ed3c9cdb96dc98f1ebaec50a0fa99beeed
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-21 01:37:29 +0200

    solve 11 task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Create class Student'''
+
+
+class Student:
+    '''made class student'''
+    def __init__(self, first_name, last_name, age) -> None:
+        '''Initialize first_name
+        last_name, age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        Reterive dict of attributes in class Student
+        Args:
+        attrs (optional, list[string])
+        Return  dictionary representation of a Student'''
+        if not attrs:
+            return self.__dict__
+        new_dict = {}
+        for attr in attrs:
+            for key in self.__dict__:
+                if key == attr:
+                    new_dict.update({key: self.__dict__[key]})
+                    break
+        return new_dict
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''dict of variables of object'''
 
+
 def class_to_json(obj: object):
     '''Return dict of obj'''
     return obj.__dict__
