b467561b08bd20c35a117dbf889efa4b9ee181bd
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-05-02 08:02:21 +0300

    add: add script that print 3 line of messages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const lastLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(lastLine);
